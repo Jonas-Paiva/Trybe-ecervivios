797282a96ff126742bf78eb44bf1fed9ed0127a0
Jonas Paiva <jonaspwork@gmail.com>
2022-06-26 12:15:11 -0300

    Revisão bloco 4 dia 1

---
+++ Fundamentos-do-Dev-Web/Bloco-04-Introdução-a-JS/dia-1-javascript-primeiros-passos/Variaveis.js
@@ -0,0 +1,17 @@
+// ==================== Exercícios ========================
+
+// Crie uma constante chamada myName e atribua a ela o seu nome (Exemplo: Carolina).
+const myName = 'Jonas';
+
+// Crie uma constante chamada birthCity e atribua a ela a sua cidade natal.
+const birthCity = 'Sobral';
+
+// Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu.
+let birthYear = 1998;
+
+// Utilize o console.log() para imprimir as constantes e variáveis que você criou.
+console.log(myName,birthCity,birthCity,birthYear);
+
+// Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!
+birthYear = 2030;
+console.log(birthYear);
---
+++ Fundamentos-do-Dev-Web/Bloco-04-Introdução-a-JS/dia-1-javascript-primeiros-passos/tiposPrimitivos.js
@@ -0,0 +1,13 @@
+// ======================== Exercícios ===========================
+
+// Crie uma constante chamada base e uma variável chamada altura e atribua os seus respectivos valores: 5 e 8.
+const base = 5;
+let altura = 8;
+
+// Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela altura. Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações!
+const area = base*altura;
+console.log(area);
+
+// Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.
+const perimeter = (base*2)+(altura*2);
+console.log(perimeter);
