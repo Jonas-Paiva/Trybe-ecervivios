fab04a99d36dec3825c782c9fec21e100fdfac9f
Jonas Paiva <jonaspwork@gmail.com>
2022-06-29 21:03:46 -0300

    laskdjflaj

--- Fundamentos-do-Dev-Web/Bloco-04-Introdução-a-JS/dia-1-javascript-primeiros-passos/ParaFixa.js
+++ Fundamentos-do-Dev-Web/Bloco-04-Introdução-a-JS/dia-1-javascript-primeiros-passos/ParaFixa.js
@@ -1,30 +1,68 @@
 // ============================= Exercícios =================================
 
 //1 Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
-// Adição (a + b)
-// Subtração (a - b)
-// Multiplicação (a * b)
-// Divisão (a / b)
-// Módulo (a % b)
+const a = 5;
+const b = 2;
 
+// Adição (a + b)
+console.log(a + b);
+
+// Subtração (a - b)
+console.log(a - b);
+
+// Multiplicação (a * b)
+console.log(a * b);
+
+// Divisão (a / b)
+console.log(a / b);
+
+// Módulo (a % b)
+console.log(a % b);
 
 //2 Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
-
+let maior = a > b ? console.log(a) : console.log(b);
 
 //3 Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
 
-
 //4 Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
-
+let n = -1;
+let PosNegaZero = n > 0 ? 'positive' : n < 0 ? 'negativo' : 'zero';
+console.log(PosNegaZero);
 
 //5 🚀 Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+
 // Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus.
 // Um ângulo será considerado inválido se não tiver um valor positivo.
+function trueTriangulo(a, b, c) {
+	if (a + b + c === 180 && a > 0 && b > 0 && c > 0) {
+		console.log('Triângulo True');
+	} else {
+		console.log('Triângulo False');
+	}
+}
+trueTriangulo(90, 45, 45);
 
 //6 Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
 // Como desafio, faça o programa funcionar tanto se receber o nome de uma peça com letras maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais.
 // Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.
 // Exemplo: bishop (bispo) -> diagonals (diagonais)
+function movimentos(pecas) {
+	switch (pecas.toLowerCase()) {
+		case 'bispo':
+			console.log('Em diagonal');
+			break;
+		case 'torre':
+			console.log('Em cruz');
+			break;
+		case 'rei':
+			console.log('Pode ir para todos lodos somente 1 casa');
+			break;
+		case 'cavalo':
+			console.log('Em L');
+			break;
+	}
+}
+movimentos('cavAlo');
 
 //7 Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
 // Porcentagem >= 90 -> A
@@ -34,45 +72,42 @@
 // Porcentagem >= 50 -> E
 // Porcentagem < 50 -> F
 // O programa deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100.
-
+function avaliação(nota, notaMaxima) {
+	let porcentagem = (nota * 100) / notaMaxima;
+	let resultado = '';
+	if (porcentagem >= 90) {
+		resultado = 'A';
+	} else if (porcentagem >= 80) {
+		resultado = 'B';
+	} else if (porcentagem >= 70) {
+		resultado = 'C';
+	} else if (porcentagem >= 60) {
+		resultado = 'D';
+	} else if (porcentagem >= 50) {
+		resultado = 'E';
+	} else if (porcentagem < 50) {
+		resultado = 'F';
+	} else if (porcentagem > 100 || porcentagem <= 0) {
+		resultado = 'ERRO';
+	}
+	return console.log(
+		`A nota é ${resultado}, de ${notaMaxima} o aluno acertou ${nota}, acertou ${porcentagem}%.`
+	);
+}
+avaliação(4, 1000);
 
 //8 🚀 Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false.
 // Bonus: use somente um if.
+let Num = [2, 3, 2345, 23, 45, 57, 46, 3, 2, 4512, 345, 234, 535];
 
+function par(a) {
+	for (const iterator of a) {
+		if (iterator % 2 === 0) {
+			console.log(`O número ${iterator} é par : True.`);
+		} else {
+			console.log(`O número ${iterator} é impar : false.`);
+		}
+	}
+}
 
-//9 Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false.
-// Bonus: use somente um if.
-
-
-//10 Escreva um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
-// Atente que, sobre o custo do produto, incide um imposto de 20%.
-// Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
-// O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
-// valorCustoTotal = valorCusto + impostoSobreOCusto
-// lucro = valorVenda - valorCustoTotal (lucro de um produto)
-
-
-//11 Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR. Faça um programa que, dado um salário bruto, calcule o líquido a ser recebido.
-// A notação para um salário de R$1500,10, por exemplo, deve ser 1500.10. Para as faixas de impostos, use as seguintes referências:
-// INSS (Instituto Nacional do Seguro Social)
-// Salário bruto até R$ 1.556,94: alíquota de 8%
-// Salário bruto de R$ 1.556,95 a R$ 2.594,92: alíquota de 9%
-// Salário bruto de R$ 2.594,93 a R$ 5.189,82: alíquota de 11%
-// Salário bruto acima de R$ 5.189,82: alíquota máxima de R$ 570,88
-// IR (Imposto de Renda)
-// Até R$ 1.903,98: isento de imposto de renda
-// De R$ 1.903,99 a 2.826,65: alíquota de 7,5% e parcela de R$ 142,80 a deduzir do imposto
-// De R$ 2.826,66 a R$ 3.751,05: alíquota de 15% e parcela de R$ 354,80 a deduzir do imposto
-// De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzir do imposto
-// Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto.
-// Exemplo: Uma pessoa possui o salário bruto de R$ 3.000,00. O cálculo será:
-// O salário bruto está entre R$ 2.594,93 e R$ 5.189,82, então sua alíquota para INSS é de 11%. O INSS será 11% de R$ 3.000, ou seja, R$ 330,00.
-// Para descobrir o salário-base, subtraia do salário bruto a alíquota do INSS: R$ 3.000,00 - R$ 330,00 = R$ 2.670,00.
-// Para pegar o valor do IR, temos um salário (já deduzido o INSS) entre R$ 1.903,99 e 2.826,65, sendo a alíquota, então, de 7.5%, com parcela de R$ 142,80 a deduzir do imposto. Assim, temos:
-// R$ 2.670,00: salário com INSS já deduzido;
-// 7.5%: alíquota de imposto de renda;
-// R$ 142,80 parcela a se deduzir do imposto.
-// Fazendo a conta, temos: (7,5% de R$ 2.670,00) - R$ 142,80 = R$ 57,45
-// O último cálculo para conseguir o salário líquido é R$ 2.670,00 - R$ 57,45 (salário-base - valor IR) = R$ 2.612,55.
-// Resultado: R$ 2.612,55.
-// Dica: que tal identificar as alíquotas com variáveis de nomes explicativos?
+par(Num);
